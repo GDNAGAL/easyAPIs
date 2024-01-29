d22dd51630cb46754e037df039e4b63f7049696f
Gordhan Suthar <gdnagal1536@gmail.com>
2024-01-29 14:28:17 +0530

    sdgsdg

--- Dashboard/getDashboardData.php
+++ Dashboard/getDashboardData.php
@@ -3,7 +3,14 @@
 
 include("../connection.php");
 require("../encryption.php");
-
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+header('Access-Control-Allow-Headers: token, Content-Type');
+header('Access-Control-Max-Age: 1728000');
+header('Content-Length: 0');
+header('Content-Type: text/plain');
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
 if ($_SERVER['REQUEST_METHOD'] == 'GET'){
 	$headers = getallheaders();
 	if (array_key_exists('Authorization', $headers) && preg_match('/Bearer\s(\S+)/', $headers['Authorization'], $matches)){
--- connection.php
+++ connection.php
@@ -24,16 +24,6 @@ if($type == "LOCALDB"){
 
 }
 
-  header('Access-Control-Allow-Origin: *');
-  header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
-  header('Access-Control-Allow-Headers: token, Content-Type');
-  header('Access-Control-Max-Age: 1728000');
-  header('Content-Length: 0');
-  header('Content-Type: text/plain');
-  header('Access-Control-Allow-Origin: *');
-  header('Content-Type: application/json');
-  die();
-
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $db);
