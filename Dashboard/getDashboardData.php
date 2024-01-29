66cc92ddd3453fea438d433329290cc2436608d6
Gordhan Suthar <gdnagal1536@gmail.com>
2024-01-29 14:36:38 +0530

    updates

--- Dashboard/getDashboardData.php
+++ Dashboard/getDashboardData.php
@@ -14,6 +14,7 @@ if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed
     header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
     header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Max-Age: 86400');
+	header("Access-Control-Allow-Headers: Authorization");
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET'){
